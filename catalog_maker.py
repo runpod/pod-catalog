fe559f5f67a665eeaf51fc9e58c8eb83a50f94c5
Justin Merrell <merrelljustin@gmail.com>
2023-08-25 17:09:56 -0400

    Update catalog_maker.py

--- catalog_maker.py
+++ catalog_maker.py
@@ -23,24 +23,32 @@ csv_data = []
 for item in data:
 
     for size in [1, 2, 4, 8]:
+        gpu = runpod.get_gpu(item['gpuTypeId'], size)
+
         new_entry = {}
 
-        print(f"Processing {item['gpuTypeId']} with {size} GPUs")
-        gpu = runpod.get_gpu(item['gpuTypeId'])
-        new_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}"
+        new_entry['InstanceType'] = ""
         new_entry['vCPUs'] = float(size * 4)
         new_entry['MemoryGiB'] = float(size * gpu['memoryInGb'])
         new_entry['AcceleratorName'] = NAME_MAP[item['gpuTypeId']]
         new_entry['AcceleratorCount'] = float(size)
         new_entry['Region'] = item['location']
-        new_entry['Price'] = gpu['lowestPrice']['uninterruptablePrice']
 
-        csv_data.append(new_entry)
+        if gpu['communityCloud'] and gpu['communityPrice'] and gpu['communitySpotPrice']:
+            new_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_COMMUNITY"
+            new_entry['Price'] = gpu['communityPrice']
+            new_entry['SpotPrice'] = gpu['communitySpotPrice']
+            csv_data.append(new_entry)
 
-print(csv_data)
+        if gpu['secureCloud'] and gpu['securePrice'] and gpu['secureSpotPrice']:
+            new_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_SECURE"
+            new_entry['Price'] = gpu['securePrice']
+            new_entry['SpotPrice'] = gpu['secureSpotPrice']
+            csv_data.append(new_entry)
+
 
 # Create a CSV file
-with open('output_file.csv', 'w') as file:
+with open('vms.csv', 'w', encoding="UTF-8") as file:
     writer = csv.DictWriter(file, fieldnames=csv_data[0].keys())
     writer.writeheader()
     writer.writerows(csv_data)
