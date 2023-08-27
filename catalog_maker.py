8c85b1a67fea8f9ab979b8d3b22d85ffc529cbb9
Justin Merrell <merrelljustin@gmail.com>
2023-08-27 18:32:35 -0400

    fix: duplication bug

--- catalog_maker.py
+++ catalog_maker.py
@@ -19,31 +19,33 @@ with open('resource_location/output_file.json', 'r') as file:
 
 csv_data = []
 for item in data:
-
     for size in [1, 2, 4, 8]:
         gpu = runpod.get_gpu(item['gpuTypeId'], size)
-
-        new_entry = {}
-
-        new_entry['InstanceType'] = ""
-        new_entry['vCPUs'] = float(size * 4)
-        new_entry['MemoryGiB'] = float(size * gpu['memoryInGb'])
-        new_entry['AcceleratorName'] = NAME_MAP[item['gpuTypeId']]
-        new_entry['AcceleratorCount'] = float(size)
-        new_entry['GpuInfo'] = gpu['displayName'].replace(" ", "_")
-        new_entry['Region'] = item['location']
+        base_entry = {
+            'InstanceType': "",
+            'vCPUs': float(size * 4),
+            'MemoryGiB': float(size * gpu['memoryInGb']),
+            'AcceleratorName': NAME_MAP[item['gpuTypeId']],
+            'AcceleratorCount': float(size),
+            'GpuInfo': gpu['displayName'].replace(" ", "_"),
+            'Region': item['location'],
+            'Price': 0.0,
+            'SpotPrice': 0.0
+        }
 
         if gpu['communityCloud'] and gpu['communityPrice'] and gpu['communitySpotPrice']:
-            new_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_COMMUNITY"
-            new_entry['Price'] = gpu['communityPrice'] * size
-            new_entry['SpotPrice'] = gpu['communitySpotPrice'] * size
-            csv_data.append(new_entry)
+            community_entry = base_entry.copy()
+            community_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_COMMUNITY"
+            community_entry['Price'] = gpu['communityPrice'] * size
+            community_entry['SpotPrice'] = gpu['communitySpotPrice'] * size
+            csv_data.append(community_entry)
 
         if gpu['secureCloud'] and gpu['securePrice'] and gpu['secureSpotPrice']:
-            new_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_SECURE"
-            new_entry['Price'] = gpu['securePrice'] * size
-            new_entry['SpotPrice'] = gpu['secureSpotPrice'] * size
-            csv_data.append(new_entry)
+            secure_entry = base_entry.copy()
+            secure_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_SECURE"
+            secure_entry['Price'] = gpu['securePrice'] * size
+            secure_entry['SpotPrice'] = gpu['secureSpotPrice'] * size
+            csv_data.append(secure_entry)
 
 
 # Create a CSV file
