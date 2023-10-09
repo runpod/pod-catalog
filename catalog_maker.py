a8c44b4ee6cbd11ed3fcb3daddb566e8690a5724
Justin Merrell <merrelljustin@gmail.com>
2023-10-09 15:30:39 -0400

    fix: only allow secure cloud

--- catalog_maker.py
+++ catalog_maker.py
@@ -1,24 +1,47 @@
+"""Create a catalog for SkyPilot
+
+get_gpu() returns a dictionary with the following keys:
+- id: The GPU ID
+- displayName: The GPU name
+- memoryInGb: The GPU memory in GB
+"""
+
 import csv
 import json
 import runpod
 
 from accelerator_naming import NAME_MAP
 
+REGION_LIST = [
+    "US", "NO", "CA", "SE", "IS", "FR", "RO", "CZ", "HR", "NL", "BG"
+]
 
 # Obtain the list of GPUs
-# gpus = runpod.get_gpus()
+gpus = runpod.get_gpus()
 
-# for gpu in gpus:
-#     print(gpu)
+
+data = []
+for gpu in gpus:
+    print(gpu)
+
+    for region in REGION_LIST:
+        data.append({
+            'gpuTypeId': gpu['id'],
+            'location': region
+        })
+
+
+# exit(0)
 
 # gpu = runpod.get_gpu("V100-SXM2-16GB")
 # print(gpu)
 
-with open('resource_location/output_file.json', 'r') as file:
-    data = json.load(file)
+# with open('resource_location/output_file.json', 'r') as file:
+#     data = json.load(file)
 
 csv_data = []
 for item in data:
+    print(item)
     for size in [1, 2, 4, 8]:
         gpu = runpod.get_gpu(item['gpuTypeId'], size)
         base_entry = {
@@ -34,17 +57,23 @@ for item in data:
         }
 
         if gpu['communityCloud'] and gpu['communityPrice'] and gpu['communitySpotPrice']:
-            community_entry = base_entry.copy()
-            community_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_COMMUNITY"
-            community_entry['Price'] = gpu['communityPrice'] * size
-            community_entry['SpotPrice'] = gpu['communitySpotPrice'] * size
-            csv_data.append(community_entry)
+            pass
+            # community_entry = base_entry.copy()
+            # community_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_COMMUNITY"
+            # community_entry['Price'] = gpu['communityPrice'] * size
+            # community_entry['SpotPrice'] = gpu['communitySpotPrice'] * size
+            # csv_data.append(community_entry)
 
-        if gpu['secureCloud'] and gpu['securePrice'] and gpu['secureSpotPrice']:
+        print(f"Secure: {gpu['secureCloud']}, {gpu['securePrice']}, {gpu['secureSpotPrice']}")
+
+        if gpu['secureCloud'] and gpu['securePrice']:
             secure_entry = base_entry.copy()
             secure_entry['InstanceType'] = f"{size}x_{NAME_MAP[item['gpuTypeId']]}_SECURE"
             secure_entry['Price'] = gpu['securePrice'] * size
-            secure_entry['SpotPrice'] = gpu['secureSpotPrice'] * size
+
+            spot_price = gpu['secureSpotPrice'] * size if gpu['secureSpotPrice'] else None
+            secure_entry['SpotPrice'] = spot_price
+
             csv_data.append(secure_entry)
 
 
